38a3728f3b7fe28b9137992074e3d4843e6b49af
Gary Burgmann <garyburgmann@gmail.com>
2017-12-31 12:55:29 +1000

    problem_10 code cleanup

--- Problem_10/problem_10.cpp
+++ Problem_10/problem_10.cpp
@@ -34,26 +34,6 @@ int main() {
   }
 
   cout << "Answer: " << sum << endl;
-  // while (num <= sqrt(roof)) {
-  //   if (num%2 != 0 && roof%num == 0) {
-  //     for (i=2; i < sqrt(num); i++) {
-  //       check = 0;
-  //       if (num % i == 0) {
-  //         check = 1;
-  //         break;
-  //       }
-  //     }
-  //     if (check == 0) {
-  //       largest = num;
-  //       cout << "Current: " << largest << endl;
-  //     }
-  //     // DEBUG cout << "Num is: " << num << " Current is: " << largest << " End Point is: " << roof/2 << endl;
-  //   }
-  //   num += 1;
-  //   // DEBUG cout << "Num is: " << num << endl;
-  // } 
-
-  // cout << "The answer is: " << largest << endl;
 
   return 0;
 }
